c729a03a7c699b05af139ccd0a9c37a889a1e295
Anesavitri <anesa.savitri22@gmail.com>
2021-12-01 22:40:36 +0700

    first commit

---
+++ welcome_message.php
@@ -0,0 +1,5 @@
+<div class="container">
+    <h1 class="mt-5 ">hi mari kita berlatih bersama Anesa</h1>
+    <p class="lead mb-5">Which is more difficult, coding or counting? Not both of them, the difficult one is sharing your knowledge with people without asking for the payment. <br/> <small>- Anesa</small></p>
+
+</div>
